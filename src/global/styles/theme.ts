abfdc861366e26b3453eddae7254d865cf7e40a6
Matheus Sales <matheus.carvalho.sales@gmail.com>
2021-10-20 21:01:29 -0300

    feat: Estilizando página de detalhamento

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -16,6 +16,17 @@ export default {
         text: '#969CB2',
         text_dark: '#000000',
         background: '#F0F2F5',
+
+        green: '#56D3B6',
+        red: '#FC7E7E',
+        blue: '#76BDFE',
+        yellow: '#FFDC7D',
+        white: '#DDDDEE',
+        brown: '#C79C85',
+        purple: '#997EFC',
+        pink: '#EDA4DC',
+        gray: '#757575',
+        black: '#414141'
     },
     fonts: {
         primary_400: 'Inter_400Regular',
@@ -25,4 +36,24 @@ export default {
         secondary_500: 'Archivo_500Medium',
         secondary_600: 'Archivo_600SemiBold',
     },
+    typeColor: {
+        grasss: '#98D071',
+        poison: '#944796',
+        bug: '#86950A',
+        dark: '#533E2F',
+        dragon: '#6B59C8',
+        eletric: '#FABE24',
+        fairy: '#F2AFF0',
+        fighting: '#833823',
+        fire: '#EC440D',
+        flying: '#ADBCF2',
+        ghost: '#6161AB',
+        ground: '#D4B662',
+        ice: '#BFECFB',
+        normal: '#C6C1B9',
+        psychic: '#EB4980',
+        rock: '#BAA35B',
+        steel: '#B8B8C5',
+        water: '#3999F9',
+    }
 }
--- src/screens/PokemonDetail/index.tsx
+++ src/screens/PokemonDetail/index.tsx
@@ -1,36 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
 import { useRoute } from '@react-navigation/native';
 
 import api from '../../service/api';
 
 import {
-    Container
+    Container,
+    Header,
+    HeaderInfo,
+    PokemonName,
+    PokemonNumber,
+    PokemonTypes,
+    TypeInfo,
+    TypeText,
+    PokemonImage,
+    ImageBackground,
+    ImageGroup
 } from './styles';
 
 interface PokemonProps {
     id: number;
     name: string;
     abilities: {
-    ability: {
-        name: string,
-    },
+        ability: {
+            name: string,
+        },
     }[];
     species: {
-    name: string
+        name: string
     };
     types: {
-    slot: number;
-    type: {
-        name: string;
-    }
+        slot: number;
+        type: {
+            name: string;
+        }
     }[];
     stats: {
-    base_stat: number;
-    stat: {
-        name: string;
-    }
+        base_stat: number;
+        stat: {
+            name: string;
+        }
     }[];
     weight: number;
     height: number;
@@ -38,7 +48,7 @@ interface PokemonProps {
 
 export function PokemonDetail() {
     const [pokemonDetails, setPokemonDetails] = useState<PokemonProps>();
-    const [ isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
     const PokemonId = useRoute<any>().params.PokemonId;
 
     async function fetchPokemon(){
@@ -57,7 +67,39 @@ export function PokemonDetail() {
     return (
         <Container>
             {
-                !isLoading && <Text>{pokemonDetails?.name}</Text>
+                !isLoading && 
+                    <>
+                    <Header>
+                    </Header>
+                        <HeaderInfo>
+                            <PokemonName>
+                                {pokemonDetails?.name}
+                            </PokemonName>
+                            <PokemonNumber>
+                                # {pokemonDetails?.id.toString().padStart(4, '0')}
+                            </PokemonNumber>
+                        </HeaderInfo>
+                        <HeaderInfo>
+                            <PokemonTypes>
+                                {pokemonDetails?.types.map(({ type }, id: number) => (
+                                    <TypeInfo value={type.name} key={`${id}-${type.name}`}>
+                                        <TypeText>{type.name}</TypeText>
+                                    </TypeInfo>
+                                ))}
+                            </PokemonTypes>
+                            
+                        </HeaderInfo>
+
+                        <ImageGroup>
+                            <PokemonImage source={{ uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails?.id}.png` }} />
+                            <ImageBackground>
+                                <Image
+                                    source={require('../../assets/pokeball.png')}
+                                    style={{ width: '100%', height: '100%' }}
+                                />
+                            </ImageBackground>
+                        </ImageGroup>
+                    </>
             }
             
         </Container>
